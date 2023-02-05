87c90e2350a4d7be7f8756b4915061854ff4124d
anatoli dyrul <dyrulan@gmail.com>
2023-02-05 23:53:59 +0300

    all tasks complit

---
+++ Task50/Program.cs
@@ -0,0 +1,90 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+пользователь вводит индексы 10, 10 - такого элемента нет.
+пользователь вводите индексы 0, 2 - выводим элеменет 7
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int rows, int collums)
+{
+    int[,] matrix = new int[rows, collums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < collums; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FindResult(int[,] matrix)
+{
+    Console.WriteLine("Введите индекс: ");
+    int indexOne = Convert.ToInt32(Console.ReadLine());
+    int indexTwo = Convert.ToInt32(Console.ReadLine());
+    if (indexOne > matrix.GetLength(0) || indexTwo > matrix.GetLength(1))
+    {
+        Console.WriteLine("Такого числа нет");
+    }
+    else
+    {
+        object result = matrix.GetValue(indexOne, indexTwo);
+        Console.WriteLine($" Число в индексе [{indexOne} {indexTwo}] равно {result}");
+    }
+
+
+}
+
+
+int countOfRows = GetNumber("Введите кол-во строк");
+int countOfCollums = GetNumber("Введите кол-во колонок");
+int[,] matrix = InitMatrix(countOfRows, countOfCollums);
+PrintMatrix(matrix);
+FindResult(matrix);
+
+
---
+++ Task57/Program.cs
@@ -0,0 +1,84 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+/*1. Проверка на число
+2.Инициализация массива
+3.Печать массива
+4.Найти среднее арифметическое элементов в первом  столбце
+5 Найти среднее арифметическое в каждом столбце*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int rows, int collums)
+{
+    int[,] matrix = new int[rows, collums];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < collums; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 44);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void GetMeanNumbersCollums(int[,] matrix)
+{
+    double sum = 0;
+    double mean = 0;
+    int count =0;
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        sum = sum + matrix[i, 0];
+
+    }
+    Console.WriteLine($"Среднее арифметическое {sum / matrix.GetLength(0)} ");
+    
+}
+
+int rows = GetNumber("Введите количество строк: ");
+int collums = GetNumber("Введите количество столбцов: ");
+int[,] matrix = InitMatrix(rows, collums);
+PrintMatrix(matrix);
+GetMeanNumbersCollums(matrix);
+
